63b63c593c9ea5273053a25f6ae6121f8053f501
Tambet Masik <tambet.masik@gmail.com>
2014-06-18 00:09:55 +0300

    Add tests for fetching methods

--- asana/asana_test.go
+++ asana/asana_test.go
@@ -44,8 +44,8 @@ func TestListWorkspaces(t *testing.T) {
 
 	mux.HandleFunc("/workspaces", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{"data":[
- 			{"id":1,"name":"Organization 1"},
- 			{"id":2,"name":"Organization 2"}
+			{"id":1,"name":"Organization 1"},
+			{"id":2,"name":"Organization 2"}
 		]}`)
 	})
 
@@ -61,5 +61,109 @@ func TestListWorkspaces(t *testing.T) {
 
 	if !reflect.DeepEqual(workspaces, want) {
 		t.Errorf("ListWorkspaces returned %+v, want %+v", workspaces, want)
+	}
+}
+
+func TestListUsers(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"data":[
+			{"id":1,"email":"test1@asana.com"},
+			{"id":2,"email":"test2@asana.com"}
+		]}`)
+	})
+
+	users, err := client.ListUsers(nil)
+	if err != nil {
+		t.Errorf("ListUsers returned error: %v", err)
+	}
+
+	want := []User{
+		{ID: 1, Email: "test1@asana.com"},
+		{ID: 2, Email: "test2@asana.com"},
+	}
+
+	if !reflect.DeepEqual(users, want) {
+		t.Errorf("ListUsers returned %+v, want %+v", users, want)
+	}
+}
+
+func TestListProjects(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"data":[
+			{"id":1,"name":"Project 1"},
+			{"id":2,"name":"Project 2"}
+		]}`)
+	})
+
+	projects, err := client.ListProjects(nil)
+	if err != nil {
+		t.Errorf("ListProjects returned error: %v", err)
+	}
+
+	want := []Project{
+		{ID: 1, Name: "Project 1"},
+		{ID: 2, Name: "Project 2"},
+	}
+
+	if !reflect.DeepEqual(projects, want) {
+		t.Errorf("ListProjects returned %+v, want %+v", projects, want)
+	}
+}
+
+func TestListTasks(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"data":[
+			{"id":1,"name":"Task 1"},
+			{"id":2,"name":"Task 2"}
+		]}`)
+	})
+
+	tasks, err := client.ListTasks(nil)
+	if err != nil {
+		t.Errorf("ListTasks returned error: %v", err)
+	}
+
+	want := []Task{
+		{ID: 1, Name: "Task 1"},
+		{ID: 2, Name: "Task 2"},
+	}
+
+	if !reflect.DeepEqual(tasks, want) {
+		t.Errorf("ListTasks returned %+v, want %+v", tasks, want)
+	}
+}
+
+func TestListTags(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/tags", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"data":[
+			{"id":1,"name":"Tag 1"},
+			{"id":2,"name":"Tag 2"}
+		]}`)
+	})
+
+	tags, err := client.ListTags(nil)
+	if err != nil {
+		t.Errorf("ListTags returned error: %v", err)
+	}
+
+	want := []Tag{
+		{ID: 1, Name: "Tag 1"},
+		{ID: 2, Name: "Tag 2"},
+	}
+
+	if !reflect.DeepEqual(tags, want) {
+		t.Errorf("ListTags returned %+v, want %+v", tags, want)
 	}
 }
